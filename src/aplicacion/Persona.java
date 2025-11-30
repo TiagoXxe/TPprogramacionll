13ddb837c8c6237eb59166acf799df56cd3bb002
TiagoXxe <tiagobarbona2@gmail.com>
2025-11-30 20:10:09 -0300

    Se creo paquete aplicacion y clases Persona, Alumno, Calificacion y Main

---
+++ src/aplicacion/Persona.java
@@ -0,0 +1,32 @@
+package aplicacion;
+
+public class Persona {
+
+    private String nombre;
+    private String apellido;
+    private int legajo;
+
+    public Persona(String nombre, String apellido, int legajo) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.legajo = legajo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public int getLegajo() {
+        return legajo;
+    }
+
+    @Override
+    public String toString() {
+        return apellido + ", " + nombre + " (Legajo: " + legajo + ")";
+    }
+
+}
